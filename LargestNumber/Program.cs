ad4ce4aff18cb95a8755bd38725e4023247ed29b
Readul Salehen <102899309+ReadulSalehen@users.noreply.github.com>
2023-08-12 21:19:50 +0600

    Add files via upload
    initial commit

---
+++ LargestNumber/Program.cs
@@ -0,0 +1,44 @@
+﻿
+//Largest Number Between 2 or 3 number
+
+// 2 numbers
+int a = 10;
+int b = 15;
+
+if (a > b)
+{
+    Console.WriteLine("a is grater");
+}
+else
+{
+    Console.WriteLine("b is grater");
+}
+
+// 3 numbers
+int c = 10;
+int d = 15;
+int e = 5;
+
+if (c > d)
+{
+    if (c > e)
+    {
+        Console.WriteLine("c is grater");
+    }
+    else
+    {
+        Console.WriteLine("e is grater");
+    }
+    
+}
+else
+{
+    if (d > e)
+    {
+        Console.WriteLine("d is grater");
+    }
+    else 
+    { 
+        Console.WriteLine("e is grater"); 
+    }
+}

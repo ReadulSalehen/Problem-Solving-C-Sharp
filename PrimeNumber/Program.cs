b9747eb2951d5a6d02e43acfd480d7041ee45b1e
Readul Salehen <102899309+ReadulSalehen@users.noreply.github.com>
2023-08-12 21:15:46 +0600

    Add files via upload
    initial commit

---
+++ PrimeNumber/Program.cs
@@ -0,0 +1,19 @@
+﻿
+for (int num = 1; num < 100; num++)
+{
+    int count = 0;
+
+    for (int i = 1; i <= num; i++)
+    {
+        if (num % i == 0)
+        {
+            count++;
+        }
+    }
+    if (count == 2)
+    {
+        
+        Console.WriteLine($"Prime Number {num}");
+    }
+}
+

fe6af182fc4cc0210cb49e20338a85c85fa5171a
Readul Salehen <102899309+ReadulSalehen@users.noreply.github.com>
2023-08-12 21:21:12 +0600

    Add files via upload
    initial commit

---
+++ ReverseANumber/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("Ente a Number:");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+int rem;
+
+
+while (num != 0)
+{
+    rem = num % 10;
+    result = result*10 + rem;
+    num = num / 10;
+}
+Console.WriteLine($"Result:- {result}");
+

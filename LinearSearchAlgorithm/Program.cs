dfd335b52f68634596fbe9221bd3eb2d607234ff
Readul Salehen <102899309+ReadulSalehen@users.noreply.github.com>
2023-08-16 00:33:48 +0600

    Add files via upload
    initial commit

---
+++ LinearSearchAlgorithm/Program.cs
@@ -0,0 +1,25 @@
+﻿
+int[] arr = new int[5] {10,20,30,40,55};
+Console.WriteLine("Enter your student id numder:");
+int studentId = int.Parse(Console.ReadLine());
+int index = LinerSearch (arr, studentId);
+if(index == 0)
+{
+    Console.WriteLine($"Student id is not found");
+}
+else
+{
+    Console.WriteLine($"Student id is found at {index} indext");
+}
+
+ int LinerSearch(int[] arr, int x)
+{
+    for(int i=0; i<arr.Length; i++)
+    {
+        if (arr[i] == x)
+        {
+            return i;
+        }
+    }
+    return -1;
+}

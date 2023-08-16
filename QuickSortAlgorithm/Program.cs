cd2138a646e933b10f504ffe2b343b5b78c624c5
Readul Salehen <102899309+ReadulSalehen@users.noreply.github.com>
2023-08-16 23:57:54 +0600

    Add files via upload
    initial commit

---
+++ QuickSortAlgorithm/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace QuickSortAlgorithm
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[] { 3, 2, 8, 4, 9 };
+            QuickSort(a, 0, a.Length - 1);
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine($"{a[i]}");
+            }
+        }
+        public static void Swap(int[] a, int i, int j)
+        {
+            int t = a[i];
+            a[i] = a[j];
+            a[j] = t;
+        }
+        private static int Partition(int[] a, int l, int r)
+        {
+            int ndx = l;
+            int pivot = a[l];
+            for (int i = l + 1; i <= r; i++)
+            {
+                if (a[i] < pivot)
+                {
+                    ndx++;
+                    Swap(a,ndx, i);
+                }
+            }
+            Swap(a, ndx, l);
+            return ndx;
+        }
+
+        private static void QuickSort(int[] a, int l, int r)
+        {
+            if (l < r)
+            {
+                var pi = Partition(a, l, r);
+                QuickSort(a, l, pi - 1);
+                QuickSort(a, l, pi +1);
+            }
+        }
+    }
+}
+
+
+
+
